618d60c60d98e084d8e7bc53872650bc6ee52a51
imrebartis <imbartis@gmail.com>
2022-01-03 14:41:11 +0200

    use the url constructor

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,36 +1,27 @@
 import * as esbuild from "esbuild-wasm";
-import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onResolve(
-        { filter: /.*/ },
-        async (args: any) => {
-          console.log("onResole", args);
-          if (args.path === "index.js") {
-            return { path: args.path, namespace: "a" };
-          }
-
-          return {
-            namespace: "a",
-            path: `https://unpkg.com/${args.path}`,
-          };
-          // else if (args.path === "tiny-test-pkg") {
-          //   return {
-          //     path: "https://unpkg-tiny-test-pkg@1.0.0/index.js",
-          //     namespace: "a",
-          //   };
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log("onResole", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
         }
 
-        //   const { data } = await axios.get(args.path);
-        //   return {
-        //     loader: "jsx",
-        //     contents: data,
-        //   };
-        // }
-      );
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            namespace: "a",
+            path: new URL(args.path, args.importer + "/").href,
+          };
+        }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log("onLoad", args);
@@ -39,7 +30,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require("tiny-test-pkg");
+              const message = require("medium-test-pkg");
               console.log(message);
             `,
           };
