5d298feedca1d99bcf1870075afedbe1a66ac2ec
imrebartis <imbartis@gmail.com>
2022-01-03 15:48:23 +0200

    resolve nested paths

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,11 +1,12 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
+        console.log("onResolve", args);
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
         }
@@ -13,7 +14,10 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes("./") || args.path.includes("../")) {
           return {
             namespace: "a",
-            path: new URL(args.path, args.importer + "/").href,
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
           };
         }
 
@@ -30,16 +34,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require("medium-test-pkg");
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
-          };
         }
+
+        const { data, request } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
       });
     },
   };
